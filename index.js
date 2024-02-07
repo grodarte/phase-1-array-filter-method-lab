74a8cb459aa47f87f60b0ea4c1529753dfa69e0b
Gabrielle <ms.grodarte@gmail.com>
2024-02-07 13:30:13 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string){
+    const listOfDrivers = array.filter(user => user.toLowerCase() === string.toLowerCase())
+    return listOfDrivers
+}
+
+function fuzzyMatch(array, letters){
+    const checkLength = letters.length
+    const listOfDrivers = array.filter(user => user.slice(0, checkLength) === letters)
+    return listOfDrivers
+}
+
+function matchName(array, string){
+    const listOfObjects = array.filter(user => user.name === string)
+    return listOfObjects
+}
